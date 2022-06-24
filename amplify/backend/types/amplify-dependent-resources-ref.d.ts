7b83f01462526b167b32d479151c843d97e2275a
Francisco Freitas Jr <fresfj@gmail.com>
2022-06-24 19:36:53 -0300

    changes for v7

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "ds3id59c473a": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "S3Trigger2ed3209f": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "s3ec3185f3": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
