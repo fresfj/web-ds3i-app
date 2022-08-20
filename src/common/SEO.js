5b8c3c3535da55ec85bc3b98a15d65ca8a801f56
Francisco Freitas Jr <fresfj@gmail.com>
2022-08-20 13:38:15 -0300

    changes for Francisco - SEO v2

--- src/common/SEO.js
+++ src/common/SEO.js
@@ -5,44 +5,41 @@ import { Person } from "schema-dts";
 import { helmetJsonLdProp } from "react-schemaorg";
 const helmetData = new HelmetData({});
 
-const SEO = ( {title} ) => {
+const SEO = ( { title } ) => {
+    const structuredJSON = JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "contactPoint" : [
+            { "@type" : "ContactPoint",
+                "telephone" : "+5541999601055",
+                "contactType" : "customer service"
+            } ],
+        "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Curitiba, Brazil",
+            "postalCode": "81560-460",
+            "streetAddress": "Eurides Maciel de Almeida, 192"
+        },
+        "member": [
+            {
+            "@type": "Organization"
+            }
+        ],
+        "alumni": [
+            {
+            "@type": "Person",
+            "name": "Francisco Freitas Jr"
+            }
+        ],
+        "url" : "https://www.ds3i.com.br",
+        "email": "francisco(at)ds3i.com.br",
+        "telephone": "+5541999601055",
+        "name": "DS3I - Desenvolvimento de Aplicativos e Sistemas"
+    });
+    
     return (
         <>
-            <Helmet helmetData={helmetData} 
-                script={[
-                    helmetJsonLdProp<Person>(
-                        {
-                            "@context": "https://schema.org",
-                            "@type": "Organization",
-                            "contactPoint" : [
-                                { "@type" : "ContactPoint",
-                                  "telephone" : "+5541999601055",
-                                  "contactType" : "customer service"
-                                } ],
-                            "address": {
-                              "@type": "PostalAddress",
-                              "addressLocality": "Curitiba, Brazil",
-                              "postalCode": "81560-460",
-                              "streetAddress": "Eurides Maciel de Almeida, 192"
-                            },
-                            "member": [
-                              {
-                                "@type": "Organization"
-                              }
-                            ],
-                            "alumni": [
-                              {
-                                "@type": "Person",
-                                "name": "Francisco Freitas Jr"
-                              }
-                            ],
-                            "url" : "https://www.ds3i.com.br",
-                            "email": "francisco(at)ds3i.com.br",
-                            "telephone": "+5541999601055",
-                            "name": "DS3I - Desenvolvimento de Aplicativos e Sistemas"
-                          }
-                    ),
-                ]}>
+            <Helmet helmetData={helmetData}>
                 <meta charSet="utf-8" />
                 <title>{title} - DS3I</title>
                 <meta name="robots" content="noindex, follow" />
@@ -53,6 +50,7 @@ const SEO = ( {title} ) => {
                 <meta name="google-site-verification" content="3JZqC_yKbhW4iMDR8mXddSBENrpOebJ7i4qhOkYT6Ak" />
                 <meta name="AdsBot-Google" content="noindex" />
                 <meta name="googlebot" content="noindex" />
+                <script className='structured-data-list' type="application/ld+json">{structuredJSON}</script>
             </Helmet>
         </>
     )
